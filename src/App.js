d647358aa1dcb5451106430af0d4f62d9e25ff34
bella <903165788@qq.com>
2019-07-14 16:27:47 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default class App extends React.Component{
+    render(){
+        return 
+        (<h2>the sky is beautiful</h2>
+               <button>学习</button>  )
+    }
+}
